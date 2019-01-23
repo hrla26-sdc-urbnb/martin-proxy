66d201071f9d1ebdf034e4aee69fb6e71d4644f5
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-23 14:40:17 -0800

    Redirect request to load balancer instance

--- proxy/router.js
+++ proxy/router.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 router.route('/units/:id')
   .get((req, res) => {
     const { id } = req.params;
-    axios.get(`http://54.211.210.36:2100/api/units/${id}`)
+    axios.get(`http://52.91.115.63:80/api/units/${id}`)
       .then(({ data }) => {
         res.send(data);
       });
