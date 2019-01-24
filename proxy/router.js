c6dbb847583a6e84b5e80bd9507d7666efe05c3c
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-23 16:48:43 -0800

    add microservice route

--- proxy/router.js
+++ proxy/router.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 router.route('/units/:id')
   .get((req, res) => {
     const { id } = req.params;
-    axios.get(`http://52.91.115.63:80/api/units/${id}`)
+    axios.get(`http://54.211.210.36:2100/api/units/${id}`) // microservice instance
+    // axios.get(`http://52.91.115.63:80/api/units/${id}`) // load balancer
       .then(({ data }) => {
         res.send(data);
       });
