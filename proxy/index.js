0a55257580f3516582ed43e360b12af73268b7a4
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-25 12:12:29 -0800

    comment out cors, redirect to load balancer

--- proxy/index.js
+++ proxy/index.js
@@ -1,8 +1,8 @@
-const newrelic = require('newrelic');
+// const newrelic = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const router = require('./router.js');
 
 const PORT = 3400;
@@ -10,7 +10,7 @@ const proxy = express();
 
 proxy.use(bodyParser.json());
 proxy.use(bodyParser.urlencoded({ extended: true }));
-proxy.use(cors());
+// proxy.use(cors());
 proxy.use(express.static(path.join(__dirname, './public/')));
 proxy.use(express.static(path.join(__dirname, './loaderio')));
 proxy.use('/api', router);
